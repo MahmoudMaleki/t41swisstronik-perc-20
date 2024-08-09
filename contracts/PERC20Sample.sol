416b2cb2508cb9c6bf50834d9d56eed480d42294
MahmoudMaleki <maleki54@gmail.com>
2024-08-09 13:54:47 +0000

    feat: initiated the project

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("javad", "jvd") {}
+    constructor() PERC20("farhad az saveh", "faseh") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
